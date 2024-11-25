928701a18041575e6f52e301e1af401ea29aa55c
Makarr <maanromanov@edu.hse.ru>
2024-11-25 10:09:19 +0300

    init and copy constructors added

--- hashtable_realizations/hashtable_realloc/ElementRealloc.hpp
+++ hashtable_realizations/hashtable_realloc/ElementRealloc.hpp
@@ -5,4 +5,8 @@ struct ElementRealloc {
     Key key;
     Value value;
     bool is_deleted = false;
+    ElementRealloc(const Key& k, const Value& v) : key(k), value(v) {};
+    ElementRealloc(const ElementRealloc& other): ElementRealloc(other.key, other.value) {
+        is_deleted(other.is_deleted);
+    };
 };
