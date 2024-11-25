cdabd1d00013e0533893045bec948cf162afc22c
Makarr <maanromanov@edu.hse.ru>
2024-11-25 10:11:37 +0300

    is_constants() -> const, load_to_file() ->const, size() -> get_size()

--- hashtable_realizations/hashtable_interface/HashTableInterface.hpp
+++ hashtable_realizations/hashtable_interface/HashTableInterface.hpp
@@ -12,10 +12,10 @@ public:
     virtual bool remove(const Key& key) = 0;
 
     virtual void clear() = 0;
-    virtual size_t size() const = 0;
-    virtual bool is_contains(const Key& key) = 0;
+    virtual size_t get_size() const = 0;
+    virtual bool is_contains(const Key& key) const = 0;
     virtual bool is_empty() const = 0;
-    virtual void load_to_file(string path) = 0;
+    virtual void load_to_file(string path) const = 0;
     virtual bool read_from_file(string path) = 0;
 
     virtual Value& operator[](const Key& key) = 0;
