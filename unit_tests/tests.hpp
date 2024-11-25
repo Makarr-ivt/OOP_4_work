c13529062aaf237c43d17e4d264445fb512909e2
Makarr <maanromanov@edu.hse.ru>
2024-11-25 05:10:50 +0300

    created tests module

---
+++ unit_tests/tests.hpp
@@ -0,0 +1,12 @@
+#ifndef __UNIT_TESTS__
+#define __UNIT_TESTS__
+
+int run_all_tests();
+int test_default_constructor();
+int test_copy_constructor();
+int test_change_in_size();
+int test_equality_operator();
+int test_intersection();
+int test_file_data_exchange();
+
+#endif
