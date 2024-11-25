15e298961ba79851fafaa227a8c41b0667c47db5
Makarr <maanromanov@edu.hse.ru>
2024-11-25 06:52:21 +0300

    test entry point

--- user_interface/main.cpp
+++ user_interface/main.cpp
@@ -1,3 +1,13 @@
+#include "tests.hpp"
+#include "ui_functions.hpp"
+#include <iostream>
+using namespace std;
+
 int main() {
+    if (run_all_tests() != 0) {
+        return 1;
+    }
+    
+    cout << "Everything is ok!\n";
     return 0;
 }
