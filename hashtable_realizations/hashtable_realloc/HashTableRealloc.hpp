4999eb3bb44fbe089e58dd33066458ff05232c47
Makarr <maanromanov@edu.hse.ru>
2024-11-25 06:57:55 +0300

    template for Hash table realloc created

--- hashtable_realizations/hashtable_realloc/HashTableRealloc.hpp
+++ hashtable_realizations/hashtable_realloc/HashTableRealloc.hpp
@@ -13,13 +13,32 @@
 #include "ElementRealloc.hpp"
 
 template<typename Key, typename Value>
-class HashTableRealloc: public HashTable {
+class HashTableRealloc: public HashTable<Key, Value> {
+public:
+    // add constructors here!!
+    ~HashTable() {}
+    bool insert(const Key& key, const Value& value) override;
+    bool remove(const Key& key) override;
+
+    void clear() override;
+    size_t size() const override;
+    bool is_contains(const Key& key) override;
+    bool is_empty() const override;
+    void load_to_file(string path) override;
+    bool read_from_file(string path) override;
+
+    Value& operator[](const Key& key) override;
+    bool operator==(const HashTable& other) const override;
+    HashTable& operator&&(const HashTable& other) override;
+    
 private:
     size_t size;
     size_t capacity;
     ElementRealloc* array;
+    void arrayRealloc();
 };
 
+// реализация методов должна быть в том же файле!
 
 
 #endif
